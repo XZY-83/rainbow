a600a6b09a1a661b30cce002e1a986451f6357e0
xzy <tnwl8279@gmail.com>
2022-05-06 00:21:21 +0900

    Dash

--- makers/Assets/Scripts/Player/Player.cs
+++ makers/Assets/Scripts/Player/Player.cs
@@ -6,9 +6,11 @@ public class Player : MonoBehaviour
 {
     [Header("Move Speed")]
     public float speed = 8f;
+    public float dashSpeed = 15f;
     public float jumpspeed = 8f;
 
-    bool grounded = false;
+    bool isGround = false;
+    public bool isDash = false;
     Rigidbody2D rigid;
     SpriteRenderer sprite;
 
@@ -20,38 +22,67 @@ public class Player : MonoBehaviour
     void Update()
     {
         HorizantalMove();
+        dash();
         Jump();
     }
 
     void HorizantalMove()  //수평움직임
     {
-        if (Input.GetAxisRaw("Horizontal")<0)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             sprite.flipX = true;
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
-        else if (Input.GetAxisRaw("Horizontal") > 0)
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             sprite.flipX = false;
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
+    }
+    void dash()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+            isDash = true;
+
+        if (isDash)
+        {
+            if (sprite.flipX)
+                transform.position += Vector3.left * speed * dashSpeed * Time.deltaTime;
+            else
+                transform.position += Vector3.right * speed * dashSpeed * Time.deltaTime;
+
+            StartCoroutine(stopDash());
+        }
+    }
+
+    IEnumerator stopDash()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isDash = false;
     }
 
     void Jump()  //점프
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (grounded)  //착지했을 때만 점프
+            if (isGround)  //착지했을 때만 점프
             {
                 rigid.velocity=new Vector2(rigid.velocity.x, jumpspeed);
-                grounded = false;
+                isGround = false;
             }
         }
+
+
+        /*if (Input.GetKeyDown(KeyCode.Space) && Input.GetButton("Vertical"))
+        {
+            GameObject.FindWithTag("DownPlatform").GetComponent<DownPlatform>().ChangeLayer();
+        }*/
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag=="Ground")
-            grounded = true;
+        if (collision.gameObject.tag == "Ground")
+            isGround = true;
     }
 }
